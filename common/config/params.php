902d6fe3fb600b9da10bbfe7556a6f410f77ee3c
shemoks <shemshur@ukr.net>
2016-05-25 10:14:31 +0300

    error correction

--- common/config/params.php
+++ common/config/params.php
@@ -3,5 +3,5 @@ return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
-    'googleApiKey'=>'AIzaSyBRJ2DjZT7emb_YBmtTxNd_rOSuB4CofSs',
+    'googleApiKey'=>'AIzaSyBRJ2DjZT7emb_YBmtTxNd_rOSuB4CofSs',//*admin.news.loc*
 ];
